1082373e41bfc00a5c4226fd4234aeb7559f4508
oviebd <oviecse48@gmail.com>
2020-07-04 14:45:03 +0600

    TEST ---- rotation function worked (need to optimize)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,20 +10,12 @@ public class PlayerMovement : MonoBehaviour
     public float rotateSpeed = 200.0f;
     public Transform target;
 
-    public float rotateSpeedAround = 200.0f;
-    public float movingSpeedAround = 5.0f;
-
     public enum movingState { MoveUp, TowardAPosition, RotateAround };
 
     public movingState currentMovingState = movingState.MoveUp;
 
-
-  
-   public float deltaAlpha = 0.01f;
-   public float a = 1f;
-
    public float radious = 1.0f;
-   float angularSpeed = 2.0f;
+   float angularSpeed = 20.0f;
    float posX, posY = 0.0f;
    float alpha = 0.0f;
 
@@ -66,46 +58,44 @@ public class PlayerMovement : MonoBehaviour
     public void MoveTowardsAposition()
     {
         Vector2 direction = (Vector2)target.position - rb.position;
-
         direction.Normalize();
-
         float rotateAmount = Vector3.Cross(direction, transform.up).z;
-
         rb.angularVelocity = -rotateAmount * rotateSpeed;
-
         rb.velocity = transform.up * movingSpeed;
     }
 
     public void MoveRotateARound()
     {
 
-       /* Vector2 center = target.position;
-        rb.velocity = Vector2.zero;
-        transform.position = new Vector2(center.x + a*Mathf.Cos(alpha), center.y + a*Mathf.Sin(alpha)) ;
-        alpha = alpha - deltaAlpha;*/
-
-        if(isRotationStart == false)
+		/* Vector2 center = target.position;
+		 rb.velocity = Vector2.zero;
+		 transform.position = new Vector2(center.x + a*Mathf.Cos(alpha), center.y + a*Mathf.Sin(alpha)) ;
+		 alpha = alpha - deltaAlpha;*/
+		rb.velocity = Vector2.zero;
+		Transform center = target;
+		if (isRotationStart == false)
         {
             isRotationStart = true;
-            alpha = Utils.GetAngle(target.position, this.gameObject.transform);
-           // alpha = alpha * Mathf.Deg2Rad;
-  
-            Debug.Log("init Angle is : " + alpha);
-        }
+            alpha = Utils.angle360(target.position, this.gameObject.transform.position) ;
+			// alpha = alpha * Mathf.Deg2Rad;
+			//radious = Utils.GetDistance(center.position, this.gameObject.transform.position);
+            Debug.Log("init Angle is : " + alpha + "  radious is  " + radious);	
+		}
 
-        Transform center = target;
+		float rad = alpha * Mathf.Deg2Rad;
+		posX = center.position.x + Mathf.Cos(rad) * radious;
+		float sinVal = Mathf.Sin(rad);
+		posY = center.position.y + sinVal * radious;
 
-        posX = center.position.x + Mathf.Cos(alpha) * radious;
-        posY = center.position.y + Mathf.Sin(alpha) * radious;
+		transform.position = new Vector2(posX, posY);
+		alpha = alpha - Time.deltaTime * angularSpeed;
+		Debug.Log(" Angle is : " + alpha);
 
-        transform.position = new Vector2(posX, posY);
-        alpha = alpha - Time.deltaTime * angularSpeed;
-        Debug.Log(" Angle is : " + alpha);
 
-        if (alpha >= 360)
-            alpha = 0;
+		//  if (alpha >= 360)
+		//   alpha = 0;
 
-        Vector3 dir = target.position - transform.position;
+		Vector3 dir = target.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis( angle, Vector3.forward);
 
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -4,36 +4,19 @@ using UnityEngine;
 
 public class Utils 
 {
-	public static float GetAngle(Vector3 target, Transform otherObj)
+	public static float angle360(Vector2 fixedPosition, Vector2 movingObjectPosition)
 	{
-		Vector2 playerRight = Vector2.up;
-		float towardsOther = (otherObj.position.y - playerRight.y) / (otherObj.position.x - playerRight.x);
-		float angle = Mathf.Atan(towardsOther) * Mathf.Rad2Deg;
-		angle = Mathf.Abs(angle);
-		int quard = GetQuardLocation(otherObj.position) - 1;
-
-		if (quard == 3)
-			angle = 360 - angle;
-		else if (quard == 1)
-			angle = 180 - angle;
-		else
-			angle = (quard * 90) + angle;
-
+		Vector2 direction = movingObjectPosition - fixedPosition;
+		Vector2 directionRoot = (new Vector2(fixedPosition.x * 2, fixedPosition.y)) - fixedPosition ;
+		float sign = (direction.y >= 0) ? 1 : -1;
+		float offset = (sign >= 0) ? 0 : 360;
+		float angle =( Vector3.Angle(directionRoot, direction) * sign) + offset;
 		return angle;
 	}
 
-	private static int GetQuardLocation(Vector3 point)
+	public static float GetDistance(Vector2 obj1, Vector2 obj2)
 	{
-		int quardNumber = 1;
-		if (point.x > 0 && point.y > 0)
-			quardNumber = 1;
-		else if (point.x > 0 && point.y < 0)
-			quardNumber = 4;
-		else if (point.x < 0 && point.y > 0)
-			quardNumber = 2;
-		else if (point.x < 0 && point.y < 0)
-			quardNumber = 3;
-
-		return quardNumber;
+		float distance = Vector3.Distance(obj1, obj2);
+		return distance;
 	}
 }
